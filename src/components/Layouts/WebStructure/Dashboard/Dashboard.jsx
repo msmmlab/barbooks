cb499c77e946ad45d875ce00ccd6ac01c929f200
msmmlab <ms@mmlab.ie>
2021-06-30 17:57:16 +1000

    searchBar component design

--- src/components/Layouts/WebStructure/Dashboard/Dashboard.jsx
+++ src/components/Layouts/WebStructure/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Dashboard.css";
 import Billboard from "../VisualElements/Billboard";
+import SearchBar from "../../../SearchEngine/SearchBar";
 
 const Dashboard = () => {
   return (
     <div className="Dashboard-container">
       <Billboard />
+      <SearchBar />
     </div>
   );
 };
---
+++ src/components/SearchEngine/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./SearchBar.css";
+import MagnifyingGlass from "../Layouts/WebStructure/Images/magnifying-glass.png";
+
+const SearchBar = () => {
+  return (
+    <div className=" SearchBar-container">
+      <div className="SearchBar-input">
+        <div className="SearchBar-input-icon">
+          <img src={MagnifyingGlass} height="20px" />
+        </div>
+        <div className="searchBar-input-input">
+          <input
+            type="text"
+            placeholder="Search by Name..."
+            name="search"
+            autoComplete="off"
+            size="40"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
