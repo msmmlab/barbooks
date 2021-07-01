0a7ab59551b399defe686ec517511711d420fa8f
msmmlab <ms@mmlab.ie>
2021-07-01 11:08:11 +1000

    GamesTable layout css

---
+++ src/components/Layouts/Games/GameCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./GameCard.css";
+
+// const game = {
+//     developer: "Phoenix Labs, Iron Galaxy",
+//     genre: "MMORPG",
+//     id: 1,
+//     platform: "PC (Windows)",
+//     publisher: "Phoenix Labs",
+//     releaseDate: "2019-05-21",
+//     shortDescription:
+//       "A free-to-play, co-op action RPG with gameplay similar to Monster Hunter.",
+//     thumbnail: "/g/1/thumbnail.jpg",
+//     title: "Dauntless",
+//   };
+
+const GameCard = ({ game }) => {
+  return (
+    <div className="GameCard">
+      <div className="GameCard-title">{game.title}</div>
+      <div className="GameCard-body">
+        <div className="GameCard-thumbnail">
+          <img src={game.thumbnail} alt={game.title} />
+        </div>
+        <div className="GameCard-content">
+          <div className="GameCard-description">{game.shortDescription}</div>
+          <div className="GameCard-link">View more {">"}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
--- src/components/Layouts/Games/GamesTable.jsx
+++ src/components/Layouts/Games/GamesTable.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import http from "../../../services/http";
 import Spinner from "../WebStructure/Images/Spinner.gif";
+import GameCard from "./GameCard";
+import "./GamesTable.css";
 
 const GamesTable = ({ platform, category, title, sorter }) => {
   const [games, setGames] = useState([]);
+
   console.log("games table platform: ", platform);
 
   useEffect(() => {
@@ -21,13 +24,25 @@ const GamesTable = ({ platform, category, title, sorter }) => {
       </div>
     );
   } else {
+    // just for testing ==> to be deleted.
+    const game = {
+      developer: "Phoenix Labs, Iron Galaxy",
+      genre: "MMORPG",
+      id: 1,
+      platform: "PC (Windows)",
+      publisher: "Phoenix Labs",
+      releaseDate: "2019-05-21",
+      shortDescription:
+        "A free-to-play, co-op action RPG with gameplay similar to Monster Hunter.",
+      thumbnail: "/g/1/thumbnail.jpg",
+      title: "Dauntless",
+    };
     return (
-      <div></div>
-      // <div>
-      //   <h1>GamesTable</h1>
-      //   <p>{game.title}</p>
-      //   <img src={game.thumbnail} />
-      // </div>
+      <div className="GameCardContainer">
+        {games.map((game) => {
+          return <GameCard game={game} key={game.id} />;
+        })}
+      </div>
     );
   }
 };
